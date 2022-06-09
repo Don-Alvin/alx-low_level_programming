ec6ae5d783b48df166f84eb9b112791af1cad9cc
Don-Alvin <alvindon41@gmail.com>
2022-06-09 23:35:58 +0300

    All tasks done

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,24 +1,45 @@
-#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
- * print_dlistint - prints a doubly linked list
- * @h: pointer to the list
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
  *
- * Return: number of nodes in the list
+ * Return: 0 on success, 1 on error
  */
-size_t print_dlistint(const dlistint_t *h)
+int main(int argc, char *argv[])
 {
-	size_t nodes = 0;
+	unsigned int i, b;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "      ";
 
-	if (!h)
-		return (0);
-
-	while (h)
+	if (argc != 2)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		nodes++;
+		printf("Correct usage: ./keygen5 username\n");
+		return (1);
 	}
-
-	return (nodes);
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0, add = 0; i < len; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	for (i = 0, b = 1; i < len; i++)
+		b *= argv[1][i];
+	p[2] = l[(b ^ 85) & 63];
+	for (b = argv[1][0], i = 0; i < len; i++)
+		if ((char)b <= argv[1][i])
+			b = argv[1][i];
+	srand(b ^ 14);
+	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
+	printf("%s\n", p);
+	return (0);
 }
